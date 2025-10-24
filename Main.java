cf7bf13454ee8bf9ec2b94ebb0409b3fc84af2a9
Чистобаев Артур Александрович и ИП-516 <chistoart.23@gmail.com>
2025-10-24 19:27:24 +0700

    Добавлен метод getCarByBrendAndYearOperational для фильтрации по марке и возрасту

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,7 +8,8 @@ public class Main {
             new Car(1, "Toyota", "Camry", 2020, "Black", 25000, "AB123CD"),
             new Car(2, "Honda", "Civic", 2019, "White", 22000, "EF456GH"),
             new Car(3, "Toyota", "Corolla", 2021, "Red", 20000, "IJ789KL"),
-            new Car(4, "BMW", "X5", 2018, "Blue", 45000, "MN012OP")
+            new Car(4, "BMW", "X5", 2018, "Blue", 45000, "MN012OP"),
+            new Car(5, "Toyota", "RAV4", 2015, "Silver", 18000, "QR345ST")
         };
 
         // Тестируем метод getCarByBrend
@@ -15,6 +17,17 @@ public class Main {
         System.out.println("Машины марки Toyota:");
         for (Car car : toyotaCars) {
             System.out.println(car.getBrand() + " " + car.getModel() + " " + car.getYear());
+        }
+
+        System.out.println("\n--- Тестирование второго метода ---");
+        
+        // Тестируем метод getCarByBrendAndYearOperational
+        Car[] oldToyotaCars = getCarByBrendAndYearOperational(cars, "Toyota", 3);
+        System.out.println("Машины Toyota старше 3 лет:");
+        for (Car car : oldToyotaCars) {
+            int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+            int age = currentYear - car.getYear();
+            System.out.println(car.getBrand() + " " + car.getModel() + " " + car.getYear() + " (возраст: " + age + " лет)");
         }
     }
 
@@ -24,6 +37,22 @@ public class Main {
         for (Car car : cars) {
             if (car.getBrand().equalsIgnoreCase(brend)) {
                 resultList.add(car);
+            }
+        }
+        
+        return resultList.toArray(new Car[0]);
+    }
+
+    public static Car[] getCarByBrendAndYearOperational(Car[] cars, String brend, int years) {
+        ArrayList<Car> resultList = new ArrayList<>();
+        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+        
+        for (Car car : cars) {
+            if (car.getBrand().equalsIgnoreCase(brend)) {
+                int carAge = currentYear - car.getYear();
+                if (carAge > years) {
+                    resultList.add(car);
+                }
             }
         }
         
